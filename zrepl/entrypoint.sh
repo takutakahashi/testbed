7a2e93e359bb0b1bf9b5a7e8d1837e873d1ab61c
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-10-29 00:03:58 +0900

    update

--- zrepl/entrypoint.sh
+++ zrepl/entrypoint.sh
@@ -3,6 +3,6 @@
 /usr/sbin/sshd
 
 while true; do
-  curl zrepl-replicator/id_rsa.pub 1>/root/.ssh/authorized_keys 2>/dev/null
+  curl replicator/id_rsa.pub 1>/root/.ssh/authorized_keys 2>/dev/null
   sleep 10
 done
--- zrepl/replicator.sh
+++ zrepl/replicator.sh
@@ -4,4 +4,5 @@ ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 mv /root/.ssh/id_rsa.pub /var/www/html/
 nginx
 sleep 40
+kubectl get pod -o wide -n zrepl |grep node-daemon|awk '{print $6," ",$7}' >> /etc/hosts
 sleep infinity
