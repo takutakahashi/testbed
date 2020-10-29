b114c4f02296c413b7b80c093626c32eff939e4d
takutaka <takutaka@pepabo.com>
2020-10-29 22:04:15 +0900

    add python app

--- zrepl/replicator.sh
+++ zrepl/replicator.sh
@@ -5,4 +5,4 @@ mv /root/.ssh/id_rsa.pub /var/www/html/
 nginx
 sleep 40
 kubectl get pod -o wide -n zrepl |grep node-daemon|awk '{print $6," ",$7}' >> /etc/hosts
-sleep infinity
+/exec_replication.py
