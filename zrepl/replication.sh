46728fc9bbdc210bba1cc191a6a57d9ece373fd8
takutaka <takutaka@pepabo.com>
2020-10-30 13:08:06 +0900

    fix bug

--- zrepl/replication.sh
+++ zrepl/replication.sh
@@ -22,7 +22,7 @@ src_first_snapshot=`ssh $SRC zfs list -t snapshot $POOL |grep -v NAME |awk '{pri
 dst_first_snapshot=`ssh $DST zfs list -t snapshot $POOL |grep -v NAME |awk '{print $1}' |sort |head -1`
 if [[ "$dst_first_snapshot" = "" ]]; then
     ssh $SRC zfs send $src_first_snapshot |ssh $DST zfs recv $POOL -F
-elif [[ "$dst_first_snapshot" != "$src_first_snapshot" ]]; then
+elif [[ `echo $dst_first_snapshot |awk -F'@' '{print $2}'` -lt `echo $src_first_snapshot |awk -F'@' '{print $2}'` ]]; then
     ssh $DST zfs destroy $dst_first_snapshot
 fi
 ssh $SRC zfs send -I $src_first_snapshot $src_last_snapshot |ssh $DST zfs recv $POOL
