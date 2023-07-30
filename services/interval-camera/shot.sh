9cf3bedf8a6cb2b23ad9f67b8a56bf9608df788d
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-07-30 23:18:13 +0900

    fix worker

--- services/interval-camera/shot.sh
+++ services/interval-camera/shot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 mount -t nfs4 disk.taka-app.net:/volume1/photo /mnt
 cd /mnt/interval
-gphoto2 --capture-image-and-download
+gphoto2 --filename %Y%m%d%H%M%S.%C --capture-image-and-download
